95a53a06ca18f056fc864e0aeafe83bfb20db578
escobedj9623 <escobedj9623@student.faytechcc.edu>
2025-11-07 19:16:10 -0500

    Add files via upload

---
+++ P1LAB1_EscobedoJacob.py
@@ -0,0 +1,11 @@
+# Jacob Escobedo
+# November 7, 2025
+# P1LAB1 - Welcome Program
+# This program asks the user for their first and last name, then displays a personalized welcome message.
+
+# Get user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("Welcome to this course,", first_name, last_name + "!")
